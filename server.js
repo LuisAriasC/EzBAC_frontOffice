b43c9427ab58f79dfe46030131a6afefa46d7f27
LuisAriasC <luis.ac945@gmail.com>
2018-11-28 04:11:42 -0600

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/src'));
+app.use(express.static('src'));
 
 app.get('/*', function(req,res) {
-	res.sendFile(path.join(__dirname+'/src/index.html'));
+	res.sendFile(path.join('src/index.html'));
 });
 
 console.log('HEY');
