9c116139aaa4eb5f2deedb20a97f47576096c9f6
LuisAriasC <luis.ac945@gmail.com>
2018-11-28 03:56:06 -0600

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/ezbac/index.html'));
 });
-
+console.log('HEY');
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
